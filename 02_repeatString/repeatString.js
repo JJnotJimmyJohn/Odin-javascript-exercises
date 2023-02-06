e6618ef3c5268250eab68f8013b0092b3c12b8c9
jianjin <jxj362@case.edu>
2023-02-05 17:20:40 -0800

    Completed 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num<0) {
+        return 'ERROR';
+    } else if (num==0) {
+        return '';
+    } else {
+        tempArray=[];
+        for (i=0;i<num;i++) {
+            tempArray.push(string);
+        }
+        return tempArray.join('');
+    }
 };
 
 // Do not edit below this line
