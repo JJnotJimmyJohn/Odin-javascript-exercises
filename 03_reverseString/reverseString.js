730af7371629407f31e9b807e9b0d48bf8b6d6bf
jianjin <jxj362@case.edu>
2023-02-05 18:42:24 -0800

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(someString) {
+    stringLength=someString.length;
+    newString='';
+    for (i=stringLength;i>0;i--) {
+        newString+=someString[i-1];
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
