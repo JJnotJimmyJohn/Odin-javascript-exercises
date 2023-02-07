911ae03eb580525712d318b75e1ec829af0bad06
jianjin <jxj362@case.edu>
2023-02-06 21:41:47 -0800

    finished exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // must be integer and positive
+    if ((year<0)||(!Number.isInteger(year))) {
+        return 'ERROR';
+    }
+    // remainder 4 is 0
+    // can't be divisible by 100 unless divisible by 400
+    if ((year%4==0)&&((year % 100 != 0)||(year%400==0))) return true;
+
+    return false;
+
 
 };
 
