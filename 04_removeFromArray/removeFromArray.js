3f2928a4322e6b351dd69a78d16686219e279cc0
jianjin <jxj362@case.edu>
2023-02-06 21:03:54 -0800

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    originArr=args.shift();
+    restArgs=args;
+    newArr=[]
+    // loop each element in newArr
+    originArr.forEach(originalElement => {
+        // if is among restArgs
+        if (restArgs.some(elementToRm => {return elementToRm===originalElement;})) {
+            console.log('Element: '+originalElement+' will not be in new arr');
+        } else {
+            //push to newArr
+            newArr.push(originalElement);
+        }
+    });
 
+    
+
+    return newArr;
 };
 
 // Do not edit below this line
